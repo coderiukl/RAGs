9a695e68a3879022c28d09c52b49148551317e9d
I'm Kratos <phucnguyenhuu6504@gmail.com>
2026-04-15 13:59:25 +0700

    Config basic

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+COLLECTION_NAME = "rag_basic"
+CHROMADB_DIR = "./chromadb"
+EMBED_MODEL = "BAAI/bge-m3"
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
+GEMINI_MODEL = "gemini-2.5-flash"
+TOP_K = 10
+MIN_SCORE = 0.4
+PDF_PATH = "./documents/6quan-tri-tai-chinh.pdf"
